3bbc02aabf7208457620521971d8bb261fc6264e
Federico Rapetti <fuji1097@gmail.com>
2018-03-07 15:17:25 +0100

    Base uploaded

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private bool spacePressed;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+		spacePressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump") && !spacePressed) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, force), ForceMode2D.Impulse);
+			spacePressed = true;
+		} else {
+			spacePressed = false;
+		}
+	}
+}
